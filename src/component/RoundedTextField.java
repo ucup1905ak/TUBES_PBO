73b37bef79f9fa34be20ec0ba5cbb044cce28227
Dion <240712797@students.uajy.ac.id>
2026-06-02 13:09:32 +0700

    main view - panel

---
+++ src/component/RoundedTextField.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package component;
+
+import java.awt.Graphics;
+import java.awt.Shape;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author aldio
+ */
+public class RoundedTextField extends JTextField {
+    
+    private Shape shape;
+    
+    public RoundedTextField(int size) {
+        super(size);
+        setOpaque(false);
+    }
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        g.setColor(getBackground());
+        g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 10, 10);
+        super.paintComponent(g);
+        
+    }
+    
+    @Override
+    protected void paintBorder(Graphics g) {
+        if (hasFocus()) {
+            g.setColor(new java.awt.Color(237, 237, 244));
+        } else {
+            g.setColor(new java.awt.Color(237, 237, 244));
+        }
+        g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 10, 10);
+    }
+}
---
+++ src/panel/LoginPagePanel.java
@@ -0,0 +1,185 @@
+package panel;
+
+import component.*;
+
+public class LoginPagePanel extends javax.swing.JPanel {
+    
+    public LoginPagePanel() {
+        initComponents();
+        
+    }
+
+    /*
+        METHOD
+     */
+    
+    private java.awt.event.ActionListener onRegistListener;
+    
+    public void setOnRegisterListener(java.awt.event.ActionListener listener){
+        this.onRegistListener = listener;
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        backgroundPanel = new javax.swing.JPanel();
+        masukButton = new javax.swing.JButton();
+        KataSandiTextField = new RoundedTextField(30);
+        namaPenggunaTextField = new RoundedTextField(30);
+        buatAkunLabel = new javax.swing.JLabel();
+        askLabel = new javax.swing.JLabel();
+        welcomeLabel = new javax.swing.JLabel();
+        userIcon = new javax.swing.JLabel();
+        background = new javax.swing.JLabel();
+
+        backgroundPanel.setBackground(new java.awt.Color(255, 255, 255));
+        backgroundPanel.setPreferredSize(new java.awt.Dimension(1280, 720));
+        backgroundPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        masukButton.setBackground(new java.awt.Color(167, 14, 45));
+        masukButton.setFont(new java.awt.Font("Arial", 1, 24)); // NOI18N
+        masukButton.setForeground(new java.awt.Color(237, 237, 244));
+        masukButton.setText("Masuk");
+        backgroundPanel.add(masukButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(426, 460, 426, 37));
+
+        KataSandiTextField.setBackground(new java.awt.Color(164, 164, 164));
+        KataSandiTextField.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        KataSandiTextField.setForeground(new java.awt.Color(237, 237, 244));
+        KataSandiTextField.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        KataSandiTextField.setText("Kata Sandi");
+        KataSandiTextField.setBorder(javax.swing.BorderFactory.createEmptyBorder(2, 10, 2, 10));
+        KataSandiTextField.setMargin(new java.awt.Insets(2, 15, 2, 6));
+        KataSandiTextField.setPreferredSize(new java.awt.Dimension(426, 37));
+        KataSandiTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                KataSandiTextFieldFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                KataSandiTextFieldFocusLost(evt);
+            }
+        });
+        backgroundPanel.add(KataSandiTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(426, 410, 426, 37));
+
+        namaPenggunaTextField.setBackground(new java.awt.Color(164, 164, 164));
+        namaPenggunaTextField.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        namaPenggunaTextField.setForeground(new java.awt.Color(237, 237, 244));
+        namaPenggunaTextField.setHorizontalAlignment(javax.swing.JTextField.LEFT);
+        namaPenggunaTextField.setText("Nama Pengguna");
+        namaPenggunaTextField.setBorder(javax.swing.BorderFactory.createEmptyBorder(2, 10, 2, 10));
+        namaPenggunaTextField.setMargin(new java.awt.Insets(2, 15, 2, 6));
+        namaPenggunaTextField.setPreferredSize(new java.awt.Dimension(426, 37));
+        namaPenggunaTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                namaPenggunaTextFieldFocusGained(evt);
+            }
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                namaPenggunaTextFieldFocusLost(evt);
+            }
+        });
+        backgroundPanel.add(namaPenggunaTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(426, 363, 426, 37));
+
+        buatAkunLabel.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        buatAkunLabel.setForeground(new java.awt.Color(33, 148, 255));
+        buatAkunLabel.setText("Buat Akun");
+        buatAkunLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                buatAkunLabelMouseClicked(evt);
+            }
+        });
+        backgroundPanel.add(buatAkunLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 300, -1, 30));
+
+        askLabel.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        askLabel.setForeground(new java.awt.Color(164, 164, 164));
+        askLabel.setText("Belum memiliki akun?");
+        backgroundPanel.add(askLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 300, -1, -1));
+
+        welcomeLabel.setBackground(new java.awt.Color(20, 20, 20));
+        welcomeLabel.setFont(new java.awt.Font("Arial", 1, 32)); // NOI18N
+        welcomeLabel.setForeground(new java.awt.Color(20, 20, 20));
+        welcomeLabel.setText("Selamat Datang,");
+        backgroundPanel.add(welcomeLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(517, 256, -1, -1));
+
+        userIcon.setBackground(new java.awt.Color(255, 255, 255));
+        userIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/LoginPage/avatar_icon.png"))); // NOI18N
+        backgroundPanel.add(userIcon, new org.netbeans.lib.awtextra.AbsoluteConstraints(608, 187, -1, -1));
+
+        background.setBackground(new java.awt.Color(255, 255, 255));
+        background.setFont(new java.awt.Font("Arial", 0, 24)); // NOI18N
+        background.setForeground(new java.awt.Color(33, 148, 255));
+        background.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/LoginPage/Vector.png"))); // NOI18N
+        background.setOpaque(true);
+        background.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                backgroundMouseClicked(evt);
+            }
+        });
+        backgroundPanel.add(background, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(backgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(backgroundPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void buatAkunLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buatAkunLabelMouseClicked
+        if(onRegistListener != null){
+            onRegistListener.actionPerformed(null);
+        }
+    }//GEN-LAST:event_buatAkunLabelMouseClicked
+
+    private void namaPenggunaTextFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_namaPenggunaTextFieldFocusGained
+        if (namaPenggunaTextField.getText().equals("Nama Pengguna")) {
+            namaPenggunaTextField.setText("");
+            namaPenggunaTextField.setForeground(new java.awt.Color(20,20,20));
+        }
+    }//GEN-LAST:event_namaPenggunaTextFieldFocusGained
+
+    private void namaPenggunaTextFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_namaPenggunaTextFieldFocusLost
+        if (namaPenggunaTextField.getText().equals("")) {
+            namaPenggunaTextField.setText("Nama Pengguna");
+            namaPenggunaTextField.setForeground(new java.awt.Color(237, 237, 244));
+        }
+    }//GEN-LAST:event_namaPenggunaTextFieldFocusLost
+
+    private void backgroundMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_backgroundMouseClicked
+        backgroundPanel.requestFocusInWindow();
+    }//GEN-LAST:event_backgroundMouseClicked
+
+    private void KataSandiTextFieldFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_KataSandiTextFieldFocusGained
+        if(KataSandiTextField.getText().equals("Kata Sandi")){
+            KataSandiTextField.setText("");
+            KataSandiTextField.setForeground(new java.awt.Color(20,20,20));
+        }
+    }//GEN-LAST:event_KataSandiTextFieldFocusGained
+
+    private void KataSandiTextFieldFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_KataSandiTextFieldFocusLost
+        if(KataSandiTextField.getText().equals("")){
+            KataSandiTextField.setText("Kata Sandi");
+            KataSandiTextField.setForeground(new java.awt.Color(237, 237, 244));
+        }
+    }//GEN-LAST:event_KataSandiTextFieldFocusLost
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField KataSandiTextField;
+    private javax.swing.JLabel askLabel;
+    private javax.swing.JLabel background;
+    private javax.swing.JPanel backgroundPanel;
+    private javax.swing.JLabel buatAkunLabel;
+    private javax.swing.JButton masukButton;
+    private javax.swing.JTextField namaPenggunaTextField;
+    private javax.swing.JLabel userIcon;
+    private javax.swing.JLabel welcomeLabel;
+    // End of variables declaration//GEN-END:variables
+}
